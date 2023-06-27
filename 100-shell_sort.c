a1b13fed836f43e5baf48835f32e1d5d97fbe6ea
mikechege01 <mike@mail.com>
2023-06-27 09:30:00 -0700

    Implementing the printing of the array once i decrease the interval

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -2,21 +2,22 @@
 #include "sort.h"
 
 /**
- * swap_array - swaps two adjacent elements in the array
- * @ele_i: The first element to swap with ele_j
- * @ele_j: The second element to swap with ele_i
+ * swap_array - swaps the two elements in the array
+ * @ele_i: The first element to swap
+ * @ele_j: The second element to swap
  */
 void swap_array(int *ele_i, int *ele_j)
 {
-        int temp;
+	int temp;
 
-        temp = *ele_i;
-        *ele_i = *ele_j;
-        *ele_j = temp;
+	temp = *ele_i;
+	*ele_i = *ele_j;
+	*ele_j = temp;
 }
 
 /**
- * shell_sort - sorts an array of elements
+ * shell_sort - sorts an array of integers -
+ * using the the Knuth sequence
  * @array: The array to be sorted
  * @size: The size of the array
  */
@@ -24,7 +25,9 @@ void shell_sort(int *array, size_t size)
 {
 	size_t start, i, j;
 
-	if (array == NULL || size < 2)
+	if (array == NULL)
+		return;
+	if (size < 2)
 		return;
 
 	for (start = 1; start < (size / 3);)
